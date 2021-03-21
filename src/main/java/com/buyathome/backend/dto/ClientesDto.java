1346a62010022746f7598722e70071b7791d0d5c
Deny Michel <dennys.michel@ucb.edu.bo>
2021-03-20 22:51:37 -0400

    Creando dependencias

---
+++ src/main/java/com/buyathome/backend/dto/ClientesDto.java
@@ -0,0 +1,23 @@
+package com.buyathome.backend.dto;
+
+import com.buyathome.backend.domain.Clientes;
+
+public class ClientesDto {
+
+    private Integer id_cliente;
+    private String correo;
+    private String password;
+    private Integer estado;
+    private String nombres;
+    private String apellidos;
+    private Date fecha_nacimiento;
+    private String telefono;
+    private String direccion;
+
+    public ClientesDto() {
+    }
+    public ClientesDto(Clientes clinetes){
+
+    }
+
+}
