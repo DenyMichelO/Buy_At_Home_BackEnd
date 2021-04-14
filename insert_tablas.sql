7da5e12b50a25d01602805e04989a10847dc0833
unknown <eddie.alba.lc@gmail.com>
2021-04-14 00:25:07 -0400

    añadiendo inserts de las BD y corrigiendo errores de backend

--- insert_tablas.sql
+++ insert_tablas.sql
@@ -4,7 +4,12 @@ insert into roles values
 ('encargado de envios');
 
 
-insert into usuarios values(1,'admin','admin',1,'Administrador','Administrador','administrador@administrador.com',0,0);
+insert into usuarios (username, password, estado, nombres, apellidos, email,ci,telefono) values
+('admin','$2a$10$gWCPSP9xPe2SoyuL47Hs5eAdI05Rra9uTEhjaHfUhrfXBBCY/ZMJi',true,'admin','admin','admin@admin.com','123456','12345678'),
+('venta','$2a$10$6lYpJTqIl/CzkFe6dw2nJ.X.dYFgL0iKWuOFt473Kxsw4QxVs/xf2',true,'venta','venta','venta@venta.com','123456','12345678');
+
+insert into roles_usuarios values(1,1),(2,2);
+
 /*inser de la tabla clientes*/
 select*from clientes;
 INSERT INTO "clientes" (correo,password,estado,nombres,apellidos,fecha_nacimiento,telefono,direccion) VALUES ('amet@enim.org','RQY12DDV7NE','1','Whilemina','Roach','23/07/2002','+591-294-69-393','293 Eleifend Av.');
