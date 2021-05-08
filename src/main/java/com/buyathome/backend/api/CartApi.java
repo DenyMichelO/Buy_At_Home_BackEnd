7ecc6b0ebf10cfa30d964f820093d173e795595b
unknown <eddie.alba.lc@gmail.com>
2021-05-08 02:02:28 -0400

    añadiendo api en el backend de cart

---
+++ src/main/java/com/buyathome/backend/api/CartApi.java
@@ -0,0 +1,68 @@
+package com.buyathome.backend.api;
+
+import bo.ucb.edu.ingsoft.bl.CartBl;
+import bo.ucb.edu.ingsoft.bl.TransactionBl;
+import com.buyathome.backend.dto.*;
+import com.buyathome.backend.model.*;
+import bo.ucb.edu.ingsoft.util.TransactionUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+
+public class CartApi {
+    private TransactionBl transactionBl;
+    private CartBl cartBl;
+
+
+    @Autowired
+    public CartApi(TransactionBl transactionBl, CartBl cartBl){
+        this.transactionBl = transactionBl;
+        this.cartBl = cartBl;
+    }
+
+    @RequestMapping(value = "/cartDetail", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes= MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
+    public CartDetailAdd createCartDetail(@Valid @RequestBody CartDetailAdd cartDetailAdd, CartDetail cartDetail, Product product, Cart cart, HttpServletRequest request){
+        Transaction transaction = TransactionUtil.createTransaction(request);
+
+        transactionBl.createTransaction(transaction);
+        CartDetailAdd cartResponse = cartBl.createCartDetail(cartDetailAdd,cartDetail,product,cart, transaction);
+        return cartResponse;
+    }
+
+    @RequestMapping(value = "/cartDetail", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
+    public List<CartGetDetail> getCartDetail(HttpServletRequest request) {
+        return cartBl.getCartDetail();
+    }
+
+    @RequestMapping(value = "/cart", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
+    public List<CartGet> getCart(HttpServletRequest request ) {
+        return cartBl.getCart();
+    }
+
+    @RequestMapping(value = "/cart", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
+    public CartAdd createCart(@RequestBody  Cart cart,CartAdd cartAdd, HttpServletRequest request){
+        Transaction transaction = TransactionUtil.createTransaction(request);
+
+        transactionBl.createTransaction(transaction);
+        CartAdd cartResponse = cartBl.createCart(cart, transaction,cartAdd);
+        return cartResponse;
+    }
+
+    @RequestMapping(value = "/cartDetail/{cartDetailId}", method = RequestMethod.PATCH, produces = MediaType.APPLICATION_JSON_VALUE, consumes= MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
+    public CartDetailDelete deleteCartDetail ( @RequestBody CartDetail cartDetail, CartDetailDelete cartDetailDelete, HttpServletRequest request, @PathVariable int cartDetailId){
+
+        CartDetailDelete productResponse = cartBl.deleteCartDetail(cartDetailDelete, cartDetailId);
+        return productResponse;
+    }
+}
---
+++ src/main/java/com/buyathome/backend/api/ProductApi.java
@@ -0,0 +1,101 @@
+package com.buyathome.backend.api;
+
+import bo.ucb.edu.ingsoft.bl.ProductBl;
+import bo.ucb.edu.ingsoft.bl.TransactionBl;
+import com.buyathome.backend.dto.*;
+import com.buyathome.backend.model.Brand;
+import com.buyathome.backend.model.Product;
+import com.buyathome.backend.model.Tag;
+import bo.ucb.edu.ingsoft.util.TransactionUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductApi {
+    private TransactionBl transactionBl;
+    private ProductBl productBl;
+
+    @Autowired
+    public ProductApi(TransactionBl transactionBl,ProductBl productBl){
+        this.transactionBl = transactionBl;
+        this.productBl = productBl;
+    }
+
+    @RequestMapping( method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes= MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
+    public ProductAdd createProduct(@Valid @RequestBody ProductAdd productAdd, Product product, Brand brand, Tag tag, HttpServletRequest request){
+        Transaction transaction = TransactionUtil.createTransaction(request);
+
+        transactionBl.createTransaction(transaction);
+        ProductAdd productResponse = productBl.createProduct(productAdd, product,brand,tag, transaction);
+        return productResponse;
+    }
+
+    /*
+    * Este Endpoint sirve para editar el precio y el stock de un porductos en la base de datos. utilizando como variable de busqueda el 'productId'.
+    */
+    @RequestMapping(value = "/{productId}", method = RequestMethod.PATCH, produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
+    public ProductUpdate updateProduct(@Valid @RequestBody ProductUpdate productUpdate,Product product, HttpServletRequest request, @PathVariable int productId){
+        Transaction transaction = TransactionUtil.createTransaction(request);
+
+        transactionBl.createTransaction(transaction);
+        ProductUpdate productResponse = productBl.updateProduct(productUpdate,transaction, product, productId);
+        return productResponse;
+    }
+    /*
+    * Este endpoint sirve para realizar el eliminado logico de un producto. Utilizando como variable 'productId'.
+    */
+    @RequestMapping(value = "/{productId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ProductDelete deleteProduct (@RequestBody ProductDelete productDelete, Product product, HttpServletRequest request, @PathVariable int productId){
+        Transaction transaction = TransactionUtil.createTransaction(request);
+
+        transactionBl.createTransaction(transaction);
+        ProductDelete productResponse = productBl.deleteProduct(productDelete, transaction, product, productId);
+        return productResponse;
+    }
+    /*
+    * Este endpoint sirve para mostra un producto. utilizando como variable de busqueda 'productId'.
+    */
+    @RequestMapping(value = "/{productId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
+    public List<ProductGetId> getProductId(HttpServletRequest request, @PathVariable int productId ) {
+        return productBl.getProductId(productId);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
+    public List<ProductsGetAdmin> getProdducts(HttpServletRequest request){
+        return productBl.getProducts();
+    }
+
+    @RequestMapping(value = "/category/{categoryId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
+    public List<ProductCategory> getProductCategory(HttpServletRequest request, @PathVariable int categoryId ) {
+        return productBl.getProductCategory(categoryId);
+    }
+
+    @RequestMapping(value = "/detail/{productId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
+    public List<ProductGetDetail> getProductDetail(HttpServletRequest request, @PathVariable int productId ) {
+        return productBl.getProductDeatil(productId);
+    }
+
+    @RequestMapping(value = "/brand", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
+    public List<BrandGet> getBrand(HttpServletRequest request) {
+        return productBl.getBrand();
+    }
+
+    @RequestMapping(value = "/category", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
+    public List<CategoryGet> getCategory(HttpServletRequest request) {
+        return productBl.getCategory();
+    }
+}
