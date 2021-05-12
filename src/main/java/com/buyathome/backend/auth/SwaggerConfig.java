8ad8912fc80b167ba1c9915f4718864ed1243f1a
DenyMichelO <dennys.michel@ucb.edu.bo>
2021-05-12 14:26:10 -0400

    Corrigiendo SwaggerConfig

--- src/main/java/com/buyathome/backend/auth/SwaggerConfig.java
+++ src/main/java/com/buyathome/backend/auth/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.codmind.swaggerapi.controllers"))
+                .apis(RequestHandlerSelectors.basePackage("com.buyathome.backend.controllers"))
                 .paths(PathSelectors.any())
                 .build();
     }
